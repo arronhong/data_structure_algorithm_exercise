aff999bdba7acd4e99328ec2fd98a8a4ba35ab8b
arronhong <32812101+arronhong@users.noreply.github.com>
2021-09-13 21:48:50 +0800

    implement binary search recursive

--- binary_search.py
+++ binary_search.py
@@ -12,6 +12,20 @@ def binary_search(arr, target):
 	return None
 
 
+def binary_search_recursive(arr, target):
+	def _binary_search(arr, start, end, target):
+		if start > end:
+			return None
+		mid = start + ((end - start) // 2)
+		if arr[mid] == target:
+			return mid
+		elif arr[mid] < target:
+			return _binary_search(arr, mid+1, end, target)
+		else:
+			return _binary_search(arr, start, mid-1, target)
+	return _binary_search(arr, 0, len(arr)-1, target)
+
+
 if __name__ == '__main__':
 	test_cases = [
 		(([0, 5, 7, 10, 15], 5), 1),
@@ -21,3 +35,4 @@ if __name__ == '__main__':
 	]
 	for (arr, tar), exp in test_cases:
     	assert exp == binary_search(arr, tar)
+        assert exp == binary_search_recursive(arr, tar)
