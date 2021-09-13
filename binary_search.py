5ca69cb6070b0f29de051c66dd99b707c33b603c
arronhong <32812101+arronhong@users.noreply.github.com>
2021-09-13 21:30:00 +0800

    improve binary search

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,23 @@
 def binary_search(arr, target):
-	low = -1
-	high = len(arr)
-	while low < high - 1:
-		mid = (low + high) // 2
+	low = 0
+	high = len(arr) - 1
+	while low <= high:
+		mid = low + ((high - low) // 2)
 		if arr[mid] == target:
 			return mid
 		elif arr[mid] < target:
-			low = mid
+			low = mid + 1
 		else:
-			high = mid
+			high = mid - 1
 	return None
+
+
+if __name__ == '__main__':
+	test_cases = [
+		(([0, 5, 7, 10, 15], 5), 1),
+		(([0, 5, 7, 10, 15], 11), None), 
+		(([0, 5, 7, 10, 15], 15), 4), 
+		(([0, 5, 7, 10, 15], 0), 0),
+	]
+	for (arr, tar), exp in test_cases:
+    	assert exp == binary_search(arr, tar)
